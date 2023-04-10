9829e098da80a9c6630fe058574ecb660539ab6b
jobemcamera <jobecamera@gmail.com>
2023-04-10 20:41:41 -0300

    Componente Menu; tag <Link> para função da tag <a> sem recarregar a página => SPA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"; // npm install react-router-dom localforage match-sorter sort-by
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Menu />
 			<Routes>
 				<Route path="/" element={<Inicio/>} />
 				<Route path="/sobremim" element={<SobreMim/>} />
