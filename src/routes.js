4068a69538d3a4a6fde26dcad99860c9cb6345d1
jobemcamera <jobecamera@gmail.com>
2023-04-11 20:00:14 -0300

    componente Rodape e criação ReactComponent para importar um svg

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"; // npm install 
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
 
 function AppRoutes() {
 	return (
@@ -12,6 +13,7 @@ function AppRoutes() {
 				<Route path="/sobremim" element={<SobreMim/>} />
 				<Route path="*" element={<div>Página não encontrada</div>} />
 			</Routes>
+			<Rodape />
 		</BrowserRouter>
 	);
 }
