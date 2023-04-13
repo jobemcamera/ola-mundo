1e16a2da81f59a541baec6bf480e294ed546f773
jobemcamera <jobecamera@gmail.com>
2023-04-13 18:21:22 -0300

    aninhamento de rotas; Componente OutLet.

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import Banner from 'componentes/Banner';
 import styles from './Inicio.module.css';
 import projetos from 'json/projetos.json'
 import Projetos from 'componentes/Projetos';
 
 export default function Inicio() {
     return (
-        <main>
-            <Banner />
-            <ul className={styles.projetos}>
-                {projetos.map( (projeto) => 
-                    <li key={projeto.id}>
-                        <Projetos projeto={projeto}/>
-                    </li>
-                )}
-            </ul>
-        </main>
+        <ul className={styles.projetos}>
+            {projetos.map((projeto) =>
+                <li key={projeto.id}>
+                    <Projetos projeto={projeto} />
+                </li>
+            )}
+        </ul>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
 	return (
 		<BrowserRouter>
 			<Menu />
 			<Routes>
-				<Route path="/" element={<Inicio/>} />
-				<Route path="/sobremim" element={<SobreMim/>} />
+				<Route path="/" element={<PaginaPadrao />}>
+					<Route index	 element={<Inicio />} />
+					<Route path="sobremim" element={<SobreMim />} />
+				</Route>
 				<Route path="*" element={<div>Página não encontrada</div>} />
 			</Routes>
 			<Rodape />
