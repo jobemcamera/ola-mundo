7744709b5544b8cebdc18f4225774156630f2ef3
jobemcamera <jobecamera@gmail.com>
2023-04-16 17:32:30 -0300

    import markdown e renderização do conteúdo de cada projeto na pagina posts

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import './Post.css'
 import { useParams } from 'react-router-dom'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import fotoCapa from "assets/sobre_mim_capa.png"
+import projetos from "json/projetos.json"
+import PostModelo from 'componentes/PostModelo';
 
 export default function Post() {
     const parametros = useParams();
 
+    const projeto = projetos.find((projeto) => {
+        return projeto.id === Number(parametros.id);
+        // parametros.id é uma string
+    })
+
     return (
-        <h1>Projeto {parametros.id}</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo={projeto.titulo}
+        >
+            <div className='post-markdown-container'>
+                <ReactMarkdown>
+                    {projeto.texto}
+                </ReactMarkdown>
+            </div>
+
+        </PostModelo>
     )
 }
