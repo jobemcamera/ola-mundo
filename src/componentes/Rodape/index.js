debcda54764e5de915fa99030307e724bac46807
jobe-lumni <jobecamera@gmail.com>
2023-06-05 09:55:05 -0300

    alteracao nos links para abertura em nova aba no navegador

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,27 +1,26 @@
+import React from 'react'
 import styles from './Rodape.module.css';
 import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
-import React from 'react'
-import { Link } from 'react-router-dom';
 
 export default function Rodape() {
     return (
         <footer className={styles.rodape}>
             <section className={styles.redes}>
-                <Link to={'https://www.instagram.com/jobecamera/'}>
+                <a href="https://www.instagram.com/jobecamera/" target="_blank" rel="noopener noreferrer">
                     <BsInstagram size={25} />
-                </Link>
-                <Link to={'https://www.linkedin.com/in/jobecamera/'}>
+                </a>
+                <a href="https://www.linkedin.com/in/jobecamera/" target="_blank" rel="noopener noreferrer">
                     <BsLinkedin size={25} />
-                </Link>
-                <Link to={'https://www.github.com/jobemcamera/'}>
+                </a>
+                <a href="https://www.github.com/jobemcamera/" target="_blank" rel="noopener noreferrer">
                     <BsGithub size={25} />
-                </Link>
+                </a>
             </section>
             <section className={styles.informacoes}>
                 <p>Desenvolvido por Jobe Camera</p>
                 <p>Inspirado no projeto Olá Mundo da Alura</p>
             </section>
         </footer>
-    )
+    );
 }
